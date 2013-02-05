cea5f13ba3e75ff06b5a8b6c04a1dde67fbb5190
Denny Abraham Cheriyan <dennyac@gmail.com>
2013-02-05 11:50:15 +0530

    Dynamic Title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title_app = "Home"
   end
 
   def help
+  	@title_app = "Help"
   end
   
   def about
+  	@title_app = "About Us"
   end
   
 end
